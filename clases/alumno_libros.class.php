f24405cf3c23dc1b2984bc894d8660b2e8f386f6
Gunvolt666 <graysonbelmont@gmail.com>
2019-02-20 13:57:48 -0600

    Update alumno_libros.class.php

--- clases/alumno_libros.class.php
+++ clases/alumno_libros.class.php
@@ -10,7 +10,7 @@ class Registro extends MySQL
 {
 	public function show($info)
 	{
-		
+		$consulta = "SELECT alumnos.Nombre, libros.nombre_libro, alumno_libros.id_alumno, alumno_libros.id_libro FROM alumno_libros INNER JOIN alumnos ON alumnos.id_alumno = alumno_libros.id_alumno INNER JOIN libros ON libros.id_libro = alumno_libros.id_libro WHERE alumno_libros.status = 0;"
 	}
 }
 
