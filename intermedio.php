53d2d6036c04be625fcac60cd3f5929322b68dc7
Gunvolt666 <graysonbelmont@gmail.com>
2019-02-25 11:50:01 -0600

    Update intermedio.php

--- intermedio.php
+++ intermedio.php
@@ -1,20 +1,28 @@
 <?php
 
-include_once 'clases/usuario.class.php'
+require_once 'clases/usuarios.class.php';
 
 $userSession = new UserSession();
-$user = new User();
+$usuario = new Usuario();
 
 if (isset($_SESSION['user'])) {
 	echo "Funciona";
 }
 elseif (isset($_POST['usuario']) && isset($_POST['password'])) {
 	$userForm = $_POST['usuario'];
+	$passForm = $_POST['password'];
+	if ($user->usuarioExiste($userForm, $passForm)) {
+		echo "usuario validado";
+	}
+	else
+	{
+		echo "Algo anda mal";
+	}
 }
 
 else
 {
 	echo "Login";
-	include_once '../Screen.php';
+	include_once 'Screen.php';
 }
 ?>
